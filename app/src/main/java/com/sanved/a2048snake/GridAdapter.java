83fd6226b157b76a69fd1e4992b39c76a50ed0ad
Sanved <sanved77@gmail.com>
2019-02-10 17:36:23 -0500

    init

---
+++ app/src/main/java/com/sanved/a2048snake/GridAdapter.java
@@ -0,0 +1,116 @@
+package com.sanved.a2048snake;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class GridAdapter extends BaseAdapter {
+    private Context context;
+    private final String[] alphabets = new String[26];
+    private int nums[] = {2,4,8,16,32,64,128,256,512,1024,2048};
+    //private static int count;
+
+
+    public GridAdapter(Context context) {
+        this.context = context;
+        fillAlphabets();
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+
+            gridView = new View(context);
+
+            // get layout from grid_item.xml
+            gridView = inflater.inflate(R.layout.list_item, null);
+            CardView cv1 = gridView.findViewById(R.id.cv1);
+            switch(nums[position]){
+                case 2:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg2));
+                    break;
+                case 4:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg4));
+                    break;
+                case 8:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg8));
+                    break;
+                case 16:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg16));
+                    break;
+                case 32:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg32));
+                    break;
+                case 64:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg64));
+                    break;
+                case 128:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg128));
+                    break;
+                case 256:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg256));
+                    break;
+                case 512:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg512));
+                    break;
+                case 1024:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg1024));
+                    break;
+                case 2048:
+                    cv1.setCardBackgroundColor(ContextCompat.getColor(context, R.color.bg2048));
+                    break;
+            }
+            // set value into textview
+            TextView textView = (TextView) gridView
+                    .findViewById(R.id.tvNum);
+            if(nums[position] > 1000){
+                textView.setTextSize(18);
+            }else if(nums[position] > 100){
+                textView.setTextSize(24);
+            }else {
+                textView.setTextSize(25);
+            }
+            textView.setText("" + nums[position]);
+
+
+        } else {
+            gridView = (View) convertView;
+        }
+
+        return gridView;
+    }
+
+    @Override
+    public int getCount() {
+        return nums.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public void fillAlphabets() {
+        int i = 0;
+        char a = 'A';
+        for (i = 0, a = 'A'; a <= 'Z'; a++, i++) {
+            alphabets[i] = "" + a;
+        }
+    }
+
+}
--- app/src/main/java/com/sanved/a2048snake/MainActivity.java
+++ app/src/main/java/com/sanved/a2048snake/MainActivity.java
@@ -1,13 +1,44 @@
 package com.sanved.a2048snake;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView grid;
+    GridAdapter gAdapt;
+
+    private String[] alphabets = new String[26];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Creating and Initiating the gridView
+
+        grid = findViewById(R.id.gridView);
+
+        gAdapt = new GridAdapter(this);
+
+        grid.setAdapter(gAdapt);
+
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                // Starts the Madiin Activity displaying all the word but also sends the selected alphabet
+                // so that only the words starting with that letter appear.
+
+                Toast.makeText(MainActivity.this, "Nagdi bai - " + i, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
